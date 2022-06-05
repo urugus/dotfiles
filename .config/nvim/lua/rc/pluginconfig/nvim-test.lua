4e0ad1c6de634bd0fc07fccc94b4c7dbeca60f92
s-sakashi <suguru.sakashita@gmail.com>
2022-06-06 08:45:07 +0900

    Fixed nvim-test

--- .config/nvim/lua/rc/pluginconfig/nvim-test.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-test.lua
@@ -5,3 +5,11 @@ require("nvim-test").setup {
     ruby = "nvim-test.runners.rspec"
   }
 }
+require('nvim-test.runners.rspec'):setup {
+    command = "bundle exec rspec",                                       -- a command to run the test runner
+    term = "toggleterm",
+    -- file_pattern = "spec(\w)+(spec\.rb)$",   -- determine whether a file is a testfile
+    -- find_files = "spec(\w)+{name}(_spec\.rb)$",
+    -- filename_modifier = nil,                                                    -- modify filename before tests run (:h filename-modifiers)
+    -- working_directory = nil,                                                    -- set working directory (cwd by default)
+  }
