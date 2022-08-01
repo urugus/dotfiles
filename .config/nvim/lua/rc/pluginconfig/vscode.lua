ff576814cdb3c0abc9642909c796d7974791cf9a
s-sakashi <suguru.sakashita@gmail.com>
2022-08-01 09:25:53 +0900

    neovim: Change background trancpancy

--- .config/nvim/lua/rc/pluginconfig/vscode.lua
+++ .config/nvim/lua/rc/pluginconfig/vscode.lua
@@ -2,7 +2,7 @@
 vim.g.vscode_style = "dark"
 -- For light theme
 -- Enable transparent background
-vim.g.vscode_transparent = 0.8
+vim.g.vscode_transparent = 1
 -- Enable italic comment
 vim.g.vscode_italic_comment = 1
 -- Disable nvim-tree background color
