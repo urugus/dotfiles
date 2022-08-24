7cdf8d17947544fc8a95ce660e37b74f1924c430
urugus <suguru.sakashita@gmail.com>
2022-08-25 00:58:27 +0900

    Add neovide settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -31,3 +31,12 @@ end)
 if vim.fn.filereadable(vim.fn.expand("~/.nvim_local_init.lua")) ~= 0 then
 	dofile(vim.fn.expand("~/.nvim_local_init.lua"))
 end
+
+-- Editor Configuration
+if vim.fn.exists("g:neovide") then
+  vim.g.neovide_transparency = 0.7
+  vim.g.neovide_input_use_logo = true
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+end
