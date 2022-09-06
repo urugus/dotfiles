84640e94b44dd0cbee7d5444822ef0027cb625ef
urugus <suguru.sakashita@gmail.com>
2022-09-06 23:48:14 +0900

    neovim: specified node version

--- .config/nvim/lua/rc/pluginconfig/copilot.lua
+++ .config/nvim/lua/rc/pluginconfig/copilot.lua
@@ -1,3 +1,4 @@
+vim.g.copilot_node_command = "~/.nodenv/versions/16.13.0/bin/node"
 vim.api.nvim_set_keymap("n", "[Copilot]e", "<Cmd>Copilot enable<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "[Copilot]d", "<Cmd>Copilot disable<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "[Copilot]s", "<Cmd>Copilot status<CR>", { noremap = true })
