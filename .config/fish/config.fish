8af1c36212f56ea6c22f3e8c3c2f3f1a0a0ba14d
urugus <suguru.sakashita@gmail.com>
2022-09-20 23:23:28 +0900

    fish: fix powerline

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,10 +9,11 @@ set -gx TERM xterm-256color
 # theme
 set -g theme_color_scheme terminal-dark
 set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
-
+set -g theme_newline_cursor yes
+set -g theme_display_git_default_branch
+set -g theme_color_scheme dark
 
 # keybinds
 bind \cr peco_select_history # Bind for peco history to Ctrl+r
