ab1b31aa75476ce93a6c6c2b24c14d3ed755eefd
urugus <suguru.sakashita@gmail.com>
2023-01-12 10:42:34 +0900

    neovim: change keybind for neo-tree

--- .config/nvim/lua/rc/pluginconfig/neo-tree.lua
+++ .config/nvim/lua/rc/pluginconfig/neo-tree.lua
@@ -149,5 +149,5 @@ require("neo-tree").setup({
 		},
 	},
 })
-vim.keymap.set("n", "<C-s>", "<Cmd>NeoTreeRevealToggle<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-f>", "<Cmd>NeoTreeFloatToggle<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-s>", "<Cmd>NeoTreeRevealToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-s>", "<Cmd>NeoTreeFloatToggle<CR>", { noremap = true, silent = true })
