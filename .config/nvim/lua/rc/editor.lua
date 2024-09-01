33e02065172b7bec0544d06d179fb4d3f15acc91
urugus <suguru.sakashita@gmail.com>
2024-09-01 09:49:46 +0900

    neovim: add editor.lua

---
+++ .config/nvim/lua/rc/editor.lua
@@ -0,0 +1,18 @@
+-- ===============================
+-- Editor Configuration
+-- ===============================
+-- neovide
+if vim.fn.exists("g:neovide") then
+  vim.g.neovide_cursor_animation_length = 0.13
+  vim.g.neovide_remember_window_size = true
+  vim.g.neovide_transparency = 0.7
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 20
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  vim.g.neovide_light_angle_degree = 45
+  vim.g.neovide_light_radius = 5
+  vim.g.neovide_font = 'UDEV Gothic LG,Hack Nerd Font,Fira Code'
+  vim.g.neovide_default_font_size = 12
+end
+
