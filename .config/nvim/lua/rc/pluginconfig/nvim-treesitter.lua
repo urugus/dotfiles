aed1f6dad17def767b37b1adfc25a71be0ed54c2
urugus <suguru.sakashita@gmail.com>
2024-09-28 12:58:36 +0900

    neovim: add config for astro

--- .config/nvim/lua/rc/pluginconfig/nvim-treesitter.lua
+++ .config/nvim/lua/rc/pluginconfig/nvim-treesitter.lua
@@ -1,7 +1,19 @@
 require 'nvim-treesitter.install'.compilers = { "clang" }
 
 require("nvim-treesitter.configs").setup({
-  -- ensure_installed = "all", -- one of 'all', 'language', or a list of languages
+ ensure_installed = {
+   "astro",
+   "bash",
+   "css",
+   "html",
+   "lua",
+   "markdown",
+   "python",
+   "ruby",
+   "tsx",
+   "typescript",
+ },
+ auto_install = true,
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = {}, -- list of language that will be disabled
