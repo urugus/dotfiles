c5721285fc14098258af42fc75f088a55e51e18a
urugus <suguru.sakashita@gmail.com>
2024-11-16 10:47:00 +0900

    neovim: add plugins render-markdown

--- .config/nvim/lua/rc/pluginlist.lua
+++ .config/nvim/lua/rc/pluginlist.lua
@@ -518,6 +518,14 @@ require("lazy").setup({
     require("rc/pluginconfig/peek")
   end,
   },
+  {
+    -- Make sure to set this up properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
 
   --------------------------------
   -- Brackets
