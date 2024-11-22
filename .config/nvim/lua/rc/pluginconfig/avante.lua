a1902aa09930b57b776376695c21f5fe92527ac1
urugus <suguru.sakashita@gmail.com>
2024-11-22 10:40:59 +0900

    neovim: use copilot in avante

--- .config/nvim/lua/rc/pluginconfig/avante.lua
+++ .config/nvim/lua/rc/pluginconfig/avante.lua
@@ -1,13 +1,13 @@
 require("avante").setup({
   ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-  provider = "claude", -- Recommend using Claude
+  provider = "copilot", -- Recommend using Claude
   auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-  claude = {
-    endpoint = "https://api.anthropic.com",
-    model = "claude-3-5-sonnet-20241022",
-    temperature = 0,
-    max_tokens = 4096,
-  },
+  -- claude = {
+  --   endpoint = "https://api.anthropic.com",
+  --   model = "claude-3-5-sonnet-20241022",
+  --   temperature = 0,
+  --   max_tokens = 4096,
+  -- },
   behaviour = {
     auto_suggestions = false, -- Experimental stage
     auto_set_highlight_group = true,
