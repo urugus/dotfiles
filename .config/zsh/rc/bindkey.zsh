cd5f80e019a1e10190ba854e5d09f7e3674ad211
urugus <suguru.sakashita@gmail.com>
2024-12-23 12:36:45 +0900

    zsh: add lazygit & yazi

--- .config/zsh/rc/bindkey.zsh
+++ .config/zsh/rc/bindkey.zsh
@@ -15,5 +15,17 @@ bindkey '^S' history-incremental-pattern-search-forward
 bindkey '^x^b' fzf_git_switch_branch
 bindkey '^x^c' anyframe-widget-cdr
 bindkey '^x^k' anyframe-widget-killfzf_git_switch_branch
+
+## Git ##
+function _lazygit-widget() { lazygit }
+zle -N _lazygit-widget
+bindkey '^g' _lazygit-widget
+
+## Filer ##
+function _yazi-widget() { yazi }
+zle -N _yazi-widget
+bindkey '^f' _yazi-widget
+
+## Completion ##
 zle -N zi
 bindkey '^z' zi
