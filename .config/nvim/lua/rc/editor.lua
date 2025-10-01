049c26469b494eb2e24899961a32a755b1788a56
urugus <21075381+urugus@users.noreply.github.com>
2025-10-01 22:41:39 +0900

    neovide: fix deprecated config

--- .config/nvim/lua/rc/editor.lua
+++ .config/nvim/lua/rc/editor.lua
@@ -5,7 +5,7 @@
 if vim.fn.exists("g:neovide") then
   vim.g.neovide_cursor_animation_length = 0.13
   vim.g.neovide_remember_window_size = true
-  vim.g.neovide_transparency = 0.7
+  vim.g.neovide_opacity = 0.7
   vim.g.neovide_floating_shadow = true
   vim.g.neovide_floating_z_height = 20
   vim.g.neovide_floating_blur_amount_x = 2.0
