7e725b2dd4feefcece73f5623a34856c66688d9d
urugus <21075381+urugus@users.noreply.github.com>
2025-11-24 19:08:19 +0900

    feat(nvim): keymaps/util に set_buf ヘルパーを追加

--- .config/nvim/lua/rc/keymaps/util.lua
+++ .config/nvim/lua/rc/keymaps/util.lua
@@ -1,6 +1,6 @@
 local M = {}
 
--- 一括でキーマップを定義するためのヘルパー
+-- 一括でキーマップを定義するヘルパー
 -- maps = { {mode, lhs, rhs, opts}, ... }
 function M.set(maps)
   for _, map in ipairs(maps) do
@@ -16,6 +16,17 @@ function M.set(maps)
       end
     end
 
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+-- バッファローカルな LSP マップ用のヘルパー
+function M.set_buf(bufnr, maps)
+  for _, map in ipairs(maps) do
+    local mode, lhs, rhs, opts = map[1], map[2], map[3], map[4] or {}
+    opts.buffer = bufnr
+    opts.noremap = opts.noremap ~= false
+    opts.silent = opts.silent ~= false
     vim.keymap.set(mode, lhs, rhs, opts)
   end
 end
